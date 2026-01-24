167e8796639aeedbaeeedc8e27603237c18021d8
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-24 21:38:22 +0530

    feat: added a new component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,39 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/Logo/user.png'
+import Logo from '../assets/Logo/bird.png';
+import styles from '../Styles/NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <div className='bg-[#0F766E] flex justify-between items-center px-5'>
+    <div className='bg-[#0F766E] flex justify-between items-center py-1 px-6'>
       <div className=''>
-        <img src={Logo} alt="" width='60PX'/>
+        <Link to='/'><img src={Logo} alt="" width='50PX' /></Link>
+
       </div>
-      <div className='flex'>
-         
-
+      <div className='flex font-sans text-[#FFFFFF] gap-5'>
         <div className="relative group">
-          <span className="cursor-pointer hover:text-[#F97316]">Shop</span>
+          <span className="cursor-pointer hover:text-[#F97316] transition duration-300">
+            SHOP
+          </span>
 
-            <ul className="absolute hidden group-hover:block bg-white text-black mt-2 rounded shadow-lg w-40">
-              <li className="px-4 py-2 hover:bg-gray-100">
-                <Link to="/shop/food">Food</Link>
-              </li>
-              <li className="px-4 py-2 hover:bg-gray-100">
-                <Link to="/shop/toys">Toys</Link>
-              </li>
-              <li className="px-4 py-2 hover:bg-gray-100">
-                <Link to="/shop/drug">Medicine</Link>
-              </li>
-            </ul>
+          <ul className="absolute shadow left-0 mt-2 w-44 bg-white text-black rounded shadow-lg
+                 opacity-0 invisible group-hover:opacity-100 group-hover:visible
+                 transition-all duration-300 delay-150">
+            <li className="px-4 py-2 hover:bg-gray-200 hover:cursor-pointer hover:rounded transition"><Link to="/">Food</Link></li>
+            <li className="px-4 py-2 hover:bg-gray-200 hover:cursor-pointer transition"><Link to="/">Toys</Link></li>
+            <li className="px-4 py-2 hover:bg-gray-200 hover:cursor-pointer hover:rounded transition"><Link to="/">Drugs</Link></li>
+          </ul>
         </div>
-          <Link to="/" className="hover:text-[#F97316]">Home</Link>
-          <Link to="/new" className="hover:text-[#F97316]">New</Link>
-          <Link to="/contact" className="hover:text-[#F97316]">Contact</Link>
-          <Link to="/hospital" className="hover:text-[#F97316]">Hospital</Link>
-        </div>
-        <div>
-          <button></button>
-        </div>
+
+        <Link to="/" className="hover:text-[#F97316]">HOSPITAL</Link>
+        <Link to="/new" className="hover:text-[#F97316]">NEWS</Link>
+        <Link to="/contact" className="hover:text-[#F97316]">CONTACT</Link>
+      </div>
+      <div className={`${styles.nav} `}>
+        <input type="checkbox" className={styles.checkbox} />
+
+        <svg className={styles.icon} viewBox="0 0 100 56">
+          <use href="#menu" className={styles.line1} />
+          <use href="#menu" className={styles.line2} />
+        </svg>
+
+        <svg className={styles.hidden}>
+          <symbol viewBox="0 0 100 56" id="menu">
+            <path d="M48.33,45.6H18a14.17,14.17,0,0,1,0-28.34H78.86a17.37,17.37,0,0,1,0,34.74H42.33l-21-21.26L47.75,4" />
+          </symbol>
+        </svg>
+      </div>
     </div>
   )
 }
