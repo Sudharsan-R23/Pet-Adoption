99130b00e06000b2623fdb0b9e9f24a99593ec41
Sudharsan <sudharsanr796@gmail.com>
2026-02-07 23:08:30 +0530

    fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,38 +9,33 @@ export default function Home() {
       <NavBar />
 
       <div className="flex justify-center">
-        <div className="relative w-11/12 mt-7">
-          
-          <img
-            src={Banner}
-            alt="Banner"
-            className="w-full rounded-3xl"
-          />
+        <div className="relative w-full max-w-7xl px-4 mt-4">
+          <div className="relative overflow-hidden rounded-3xl h-[60vh] sm:h-[65vh] lg:h-[500px]">
+            <img
+              src={Banner}
+              alt="Banner"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 flex flex-col justify-between p-5 sm:p-8 lg:p-10">
+              <h1 className="text-3xl sm:text-4xl lg:text-6xl text-black">
+                hello world
+              </h1>
+              <div className="flex flex-col sm:flex-row gap-4">
+                <button className={`${styles.customCursor}
+                  bg-[#0F766E] text-white uppercase
+                  text-lg sm:text-xl
+                  px-6 py-3 sm:px-9 sm:py-5
+                  rounded-xl`}>Adaptation</button>
 
-          
-          <div
-            className="absolute inset-0 flex flex-col justify-between p-10"
-          >
-           
-            <h1 className="text-6xl max-[768px]:text-4xl">
-              hello world
-            </h1>
+                <button className={`${styles.customCursor} bg-[#0F766E] text-white uppercase
+                  text-lg sm:text-xl
+                  px-6 py-3 sm:px-9 sm:py-5
+                  rounded-xl`}
+                >
+                  Donate
+                </button>
+              </div>
 
-           
-            <div>
-              <button
-                className={`${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 py-5 rounded-xl mr-7
-                max-[1024px]:px-3 max-[1024px]:py-3 max-[768px]:text-xl`}
-              >
-                Adaptation
-              </button>
-
-              <button
-                className={`${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 py-5 rounded-xl
-                max-[1024px]:px-3 max-[1024px]:py-3 max-[768px]:text-xl`}
-              >
-                Donate
-              </button>
             </div>
           </div>
         </div>
