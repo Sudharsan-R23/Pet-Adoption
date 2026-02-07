7afbcde6af45c2508da052bc40b7a1fd61b31827
Sudharsan <sudharsanr796@gmail.com>
2026-02-07 23:01:52 +0530

    fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,21 +7,44 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className={`flex justify-center`}>
-        <div className={`flex justify-center w-11/12 relative -z-10 mt-7`}>
-          <img src={Banner} className={`rounded-3xl`} alt="" width='100%' />
-          <div className={`${styles.Itemstxt} absolute inset-0 flex flex-col justify-between p-10`}>
-            <h1 className={`text-6xl`}>hello world</h1>
-            <div className={``}>
-              <button className={` ${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 max-[1024px]:py-3 max-[768px]:text-xl max-[1024px]:px-3 py-5 rounded-xl mr-7`}>Adaptation</button>
-              <button className={` ${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 max-[1024px]:py-3 max-[768px]:text-xl max-[1024px]:px-3 py-5 rounded-xl mr-7`}>Donate</button>
+
+      <div className="flex justify-center">
+        <div className="relative w-11/12 mt-7">
+          
+          <img
+            src={Banner}
+            alt="Banner"
+            className="w-full rounded-3xl"
+          />
+
+          
+          <div
+            className="absolute inset-0 flex flex-col justify-between p-10"
+          >
+           
+            <h1 className="text-6xl max-[768px]:text-4xl">
+              hello world
+            </h1>
+
+           
+            <div>
+              <button
+                className={`${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 py-5 rounded-xl mr-7
+                max-[1024px]:px-3 max-[1024px]:py-3 max-[768px]:text-xl`}
+              >
+                Adaptation
+              </button>
+
+              <button
+                className={`${styles.customCursor} bg-[#0F766E] text-2xl uppercase text-white px-9 py-5 rounded-xl
+                max-[1024px]:px-3 max-[1024px]:py-3 max-[768px]:text-xl`}
+              >
+                Donate
+              </button>
             </div>
-
-
-
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
