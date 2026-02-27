7691c896ea6d393170ad8c44d554998b544e829c
Sudharsan <sudharsanr796@gmail.com>
2026-02-27 21:33:06 +0530

    Fix: added components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <div>
+            <h1>Create a family with us</h1>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
