129984685640e6220844ea988d48d92440fc718a
Sudharsan <sudharsanr796@gmail.com>
2026-03-09 22:54:01 +0530

    Refacter

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function
 () {
   return (
     <div>
-        <div className={`p-15 text-5xl content-center bg-[#0F766E] justify-center content-center flex text-white`}>
+        <div className={`p-15 md:text-5xl content-center bg-[#0F766E] justify-center content-center flex text-white`}>
             <h1>Create a family with us | </h1>
             <div className={`flex ml-1 pt-1 gap-1`}>
                 < TiSocialYoutube/>
