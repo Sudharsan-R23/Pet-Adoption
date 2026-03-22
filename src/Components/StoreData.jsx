2052bfd18c1131c1d755c7c91568720ec366a18c
Sudharsan <sudharsanr796@gmail.com>
2026-03-22 13:50:09 +0530

    Fix!

--- src/Components/StoreData.jsx
+++ src/Components/StoreData.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import IMG1 from '../assets/images/'
 
 export default function() {
 
     const Data=[
         {
+            Img:IMG1,
             Product:'Food',
             Detail:"Lorem ipsum dolor sit, amet consecAnimi, alias",
-            price:"$10"}
+            price:"$10"
+        }
         ]
         const storesData = Data.map((tryData)=>)
   return (
